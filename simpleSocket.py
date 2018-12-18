d7aa7c328c9ccbccbe9073d8e3df39af91267d7f
pageinsec <36606674+pageinsec@users.noreply.github.com>
2018-12-18 14:41:20 -0600

    Create simpleSocket.py

---
+++ simpleSocket.py
@@ -0,0 +1,19 @@
+# Simple Python3 server 
+
+import socket
+
+hostIP = input("Host IP: ")
+hostPort = int(input("Host Port: ")
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((hostIP, hostPort))
+server.listen(1)
+print("Server initiated. Waiting for connection.")
+connection, address = server.accept()
+print("Connection from:", address)
+while 1:
+  data = connection.recv(1024)
+  if not data: break
+  connection.sendall(b'--Message Received --\n')
+  print(data.decode('utf-8'))
+connection.close()
